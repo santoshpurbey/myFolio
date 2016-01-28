30b61dd5c8c8fcd225a7e73cb8091c1aea094ec5
Katerina Kampardi <kkampardi@gmail.com>
2016-01-28 11:20:17 +0200

    #Introduce basic app configuration and project structure, gulp, npm etc.

---
+++ app/scripts/cotrollers.js
@@ -0,0 +1,7 @@
+'use_strict';
+
+angular.module('myPholioApp')
+
+  .controller('IndexController', ['$scope', '$http', function($scope, $http){
+    console.log("Index/Home  Controller :)");
+    }])
