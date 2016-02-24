72681266a28f7eb97871e6cdba287c4da81ca377
Katerina Kampardi <kkampardi@gmail.com>
2016-02-24 19:41:50 +0200

    #add admit automated slug

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from models import Project, Category, Skill
 
+
+# set up automated slug creation
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
 # Register your models here.
 
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Category)
 admin.site.register(Skill)
