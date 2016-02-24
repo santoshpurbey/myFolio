ac960dbdad32870c16c1c21ae55c8a422ccba02d
Katerina Kampardi <kkampardi@gmail.com>
2016-02-24 23:50:54 +0200

    #edit local settings

--- portfolioapp/settings.py
+++ portfolioapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cs#n217z6ozs%7&qli@-r7r%hl2%-)x%f*%27a!hv45qom7%jf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
